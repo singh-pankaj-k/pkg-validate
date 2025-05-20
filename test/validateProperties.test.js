924df41a6a8323b4d48dee4219ede6350732f052
Pankaj <pankajpriscilla@gmail.com>
2025-05-20 08:00:35 -0400

    Permission validation added.

--- test/validateProperties.test.js
+++ test/validateProperties.test.js
@@ -26,7 +26,7 @@ describe( 'ValidateProperties test', function () {
         } );
 
         it( 'item_id uuid validate', function ( done ) {
-            const testObject = { item_id: "1c76ea46-a212-4cc5-9031-a9a28d927c4c"};
+            const testObject = { item_id: "1c76ea46-a212-4cc5-9031-a9a28d927c4c" };
 
             const validatedObject = validateProperties.validateProperties( testObject );
 
@@ -35,13 +35,31 @@ describe( 'ValidateProperties test', function () {
         } );
 
         it( 'search_string validate', function ( done ) {
-            const testObject_1 = { search_string: "1c76ea46-a212-4cc5-9031-a9a28d927c4c"};
-            const testObject_2 = { search_string: " claudpsnd@yahoo.ca "};
+            const testObject_1 = { search_string: "1c76ea46-a212-4cc5-9031-a9a28d927c4c" };
+            const testObject_2 = { search_string: " claudpsnd@yahoo.ca " };
 
             const validatedObject_1 = validateProperties.validateProperties( testObject_1 );
             const validatedObject_2 = validateProperties.validateProperties( testObject_2 );
 
             assert.deepStrictEqual( validatedObject_1, {}, 'Validation failed' );
+            assert.deepStrictEqual( validatedObject_2, testObject_2, 'Validation failed' );
+            done();
+        } );
+
+        it( 'permission_string validate', function ( done ) {
+            const testObject_1 = {
+                permission: {
+                    resource: "unit_a",
+                    name: "south_homes",
+                    type: "r"
+                }
+            };
+            const testObject_2 = { permission: { email: "claudpsnd@yahoo.ca" } };
+
+            const validatedObject_1 = validateProperties.validateProperties( testObject_1 );
+            const validatedObject_2 = validateProperties.validateProperties( testObject_2 );
+
+            assert.deepStrictEqual( validatedObject_1, testObject_1, 'Validation failed' );
             assert.deepStrictEqual( validatedObject_2, testObject_2, 'Validation failed' );
             done();
         } );
